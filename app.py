e5fb7b81983039d34f0a9ec9b391a79ef433aa74
jul1anpa <paynejulian1998@gmail.com>
2023-12-06 20:02:33 -0500

    uploaded source code -jp

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route("/home", methods=["GET", "POST"])
+def go_home():
+    if request.method == "POST":
+        artist = request.form["artist"]
+        return get_artist(artist)
+    else:
+        return render_template("home.html")
+
+@app.route("/artist/<artist>", methods=["GET"])
+def get_artist(artist):
+    url = f"https://api.deezer.com/search/artist?q={artist}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        artist_data = response.json()
+        tracklist_url = artist_data["data"][0]["tracklist"]
+        top_five_url = tracklist_url.replace("50", "5")
+        response = requests.get(top_five_url)
+        track_data = response.json()
+        tracks = [i["title"] for i in track_data["data"]]
+        return render_template("artist.html", data=tracks)
+    else:
+        return "Error fetching artist data from API"
